d4972927afbab7742f78a91f27973dd0513c9481
Yasemin <yasemin.aksoy@stu.fsm.edu.tr>
2020-12-26 21:29:42 +0300

    database connection

--- Identity/Identity/Context/AppRole.cs
+++ Identity/Identity/Context/AppRole.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Identity.Context
 {
-    public class Context
+    public class AppRole : IdentityRole<int>
     {
     }
 }
---
+++ Identity/Identity/Context/ProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Context
+{
+    public class ProjectContext : IdentityDbContext <AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=IdentityProject; integrated security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
