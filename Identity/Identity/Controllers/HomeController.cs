8bd035e50f2727020631d3b807890023b38690fe
Yasemin <yasemin.aksoy@stu.fsm.edu.tr>
2020-12-27 18:52:44 +0300

    signup completed

--- Identity/Identity/Controllers/HomeController.cs
+++ Identity/Identity/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Identity.Models;
+﻿using Identity.Context;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,11 @@ namespace Identity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View(new UserSignInViewModel());
@@ -16,6 +23,33 @@ namespace Identity.Controllers
         public IActionResult SignUp()
         {
             return View(new UserSignUpViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserSignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.UserName,
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email
+                };
+               var result = await _userManager.CreateAsync(user,model.Password);
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
--- Identity/Identity/Migrations/20201227153602_AddColumnsNameSurnameToAppUser.Designer.cs
+++ Identity/Identity/Migrations/20201227153602_AddColumnsNameSurnameToAppUser.Designer.cs
@@ -4,14 +4,16 @@ using Identity.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Identity.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20201227153602_AddColumnsNameSurnameToAppUser")]
+    partial class AddColumnsNameSurnameToAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
