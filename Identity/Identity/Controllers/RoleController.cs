c49564c3981debf5fa550b29cc2a8f2af2862222
Yasemin <yasemin.aksoy@stu.fsm.edu.tr>
2021-02-03 18:10:41 +0300

    add role

--- Identity/Identity/Controllers/RoleController.cs
+++ Identity/Identity/Controllers/RoleController.cs
@@ -1,4 +1,6 @@
 ﻿using Identity.Context;
+using Identity.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,9 +10,10 @@ using System.Threading.Tasks;
 
 namespace Identity.Controllers
 {
+    [Authorize]
     public class RoleController : Controller
     {
-        RoleManager<AppRole> _roleManager;
+        private readonly RoleManager<AppRole> _roleManager;
         public RoleController(RoleManager<AppRole> roleManager)
         {
             _roleManager = roleManager;
@@ -18,6 +21,31 @@ namespace Identity.Controllers
         public IActionResult Index()
         {
             return View(_roleManager.Roles.ToList());
+        }
+        public IActionResult AddRole()
+        {
+            return View(new RoleViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole role = new AppRole()
+                {
+                    Name = model.Name
+                };
+                var identityresult = await _roleManager.CreateAsync(role);
+                if (identityresult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var item in identityresult.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Identity/Identity/Models/RoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Enter the name")]
+        [Display(Name ="Name : ")]
+        public string Name { get; set; }
+    }
+}
