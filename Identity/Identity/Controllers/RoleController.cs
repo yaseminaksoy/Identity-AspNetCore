7217f2553e0362d5cfa863be8a84d6603b3c0939
Yasemin <yasemin.aksoy@stu.fsm.edu.tr>
2021-02-03 16:53:25 +0300

    role list

---
+++ Identity/Identity/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using Identity.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Controllers
+{
+    public class RoleController : Controller
+    {
+        RoleManager<AppRole> _roleManager;
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+    }
+}
